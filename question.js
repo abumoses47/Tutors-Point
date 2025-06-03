aeb27b1c5440066b517cc1ce81fb7f2f2b6a5947
Abubakar Moses <43930739+abumoses47@users.noreply.github.com>
2025-06-03 20:49:46 +0100

    Add files via upload

---
+++ question.js
@@ -0,0 +1,57 @@
+let questions = [
+  {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "C. Hper text markup language",
+    options: [
+        "A. Hyper type multi language",
+        "B. Hyper text multiple language",
+        "C. Hper text markup language",
+        "D. Hyper text multi language"
+    ]
+  },
+  {
+    numb: 2,
+    question: "What does CSS stand for",
+    answer: "A. Cascading style sheet",
+    options: [
+        "A. Cascading style sheet",
+        "B. Cute style sheet",
+        "C. Computer style sheet",
+        "D. Code style sheet"
+    ]
+  },
+  {
+    numb: 3,
+    question: "What does PHP stand for",
+    answer: "A. Hypertext preprocessor",
+    options: [
+        "A. Hypertext preprocessor",
+        "B. Hometext programming",
+        "C. Hypertext programming",
+        "D. Programming hypertext preprocessor"
+    ]
+  },
+  {
+    numb: 4,
+    question: "What does SQL stand for",
+    answer: "D. Structured query language",
+    options: [
+        "A. Strength query language",
+        "B. Style sheet query language",
+        "C. System query language",
+        "D. Structured query language"
+    ]
+  },
+  {
+    numb: 5,
+    question: "What does XML stand for",
+    answer: "D. Extensible markup language",
+    options: [
+        "A. Excellent multiple language",
+        "B. Explore multiple language",
+        "C. Extra markup language",
+        "D. Extensible markup language"
+    ]
+  }
+]
